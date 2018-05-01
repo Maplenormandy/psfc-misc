546e746dba89acff0cec3522b7e7876c45431104
Norman Cao <maplenormandy@gmail.com>
2018-04-30 20:28:59 -0400

    Annotate ion_plot

--- Spectroscopy/forFrancesco/ion_plot.py
+++ Spectroscopy/forFrancesco/ion_plot.py
@@ -8,13 +8,14 @@ Created on Tue Mar 22 21:48:02 2016
 import numpy as np
 import matplotlib.pyplot as plt
 
+# My python installation yells at me if I don't have this for some reason
 import readline
+readline
 import MDSplus
 
 
-#import shotAnalysisTools as sat
 
-readline
+
 
 class ThacoData:
     def __init__(self, node):
@@ -38,18 +39,23 @@ class ThacoData:
 
 specTree = MDSplus.Tree('spectroscopy', 1101014029)
 
+# Load the nodes associated with inverted profile data
 nodeA = specTree.getNode(r'\SPECTROSCOPY::TOP.HIREXSR.ANALYSIS1.HELIKE.PROFILES.Z')
 nodeB = specTree.getNode(r'\SPECTROSCOPY::TOP.HIREXSR.ANALYSIS.HLIKE.PROFILES.LYA1')
 
+# Load the actual data
 dataA = ThacoData(nodeA)
 dataB = ThacoData(nodeB)
 
 t0 = 1.01
-
+# Look for the index corresponding to the given time point
 indexA = np.searchsorted(dataA.time, t0)
 indexB = np.searchsorted(dataB.time, t0)
 
+# Plot the Ti
 plt.figure()
 plt.errorbar(dataA.rho, dataA.pro[3,indexA,:], yerr=dataA.perr[3,indexA,:])
 plt.errorbar(dataB.rho, dataB.pro[3,indexB,:], yerr=dataB.perr[3,indexB,:])
+plt.ylabel('Ti [keV]')
+plt.xlabel('normalized poloidal flux')
 plt.show()
